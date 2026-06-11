889e0d3292298a0e5beb9cb4d1efee64eb3690be
andy <scym14@gmail.com>
2026-06-11 10:34:46 +0900

    Create TennisApp.java

---
+++ tennis_game/src/tennis_game/TennisApp.java
@@ -0,0 +1,15 @@
+package tennis_game;
+
+public class TennisApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//        GameInput input = new ConsoleInput();
+//        GameOutput output = new ConsoleOutput();
+//        TennisManager manager = new TennisManager(input, output);
+//        manager.run();
+		
+		System.out.println("Tennis Game Main ");
+	}
+
+}
